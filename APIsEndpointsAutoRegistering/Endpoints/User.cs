7af8f5664ad1f93f1f67d1e8f4adf739e1561606
stefan.djokic@exlrt.com <stefan.djokic@exlrt.com>
2025-01-21 20:05:29 +0100

    Added new MapGet for Users/Test

--- APIsEndpointsAutoRegistering/Endpoints/User.cs
+++ APIsEndpointsAutoRegistering/Endpoints/User.cs
@@ -8,5 +8,6 @@ public class UserEndpoint : IEndpoint
     {
         endpoints.MapPost("/users", (string name) => Results.Ok($"User {name} created!"));
         endpoints.MapGet("/users", () => Results.Ok(new List<string> { "User 1", "User 2" }));
+        endpoints.MapGet("/users/test", () => Results.Ok("Test"));
     }
 }
